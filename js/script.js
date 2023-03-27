fe8f9f2d7d02fc3e859cf6ccc188fcb6088e14f3
mitory <lairziro@gmail.com>
2023-03-27 20:53:08 +0300

    add slider

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$('.team-slider').slick({
+		dots: true,
+		slidesToShow: 3,
+		speed: 500,
+		waitForAnimate: false,
+		responsive:[
+			// {
+			// 	breakpoint:
+			// 	settings: {}
+			// }
+		]
+	});
+});
