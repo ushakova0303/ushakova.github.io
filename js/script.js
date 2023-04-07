4333ad69d7fe9dfa2f630f90dd32f3f95f549a0b
mitory <lairziro@gmail.com>
2023-04-07 21:32:20 +0300

    update

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	$('.team-slider').slick({
 		dots: true,
 		slidesToShow: 3,
-		speed: 500,
+		speed: 350,
 		responsive:[
 			// {
 			// 	breakpoint:
@@ -11,3 +11,17 @@ $(document).ready(function(){
 		]
 	});
 });
+let isDark = true
+function changeTheme(){
+	if(isDark){
+		document.body.style.setProperty('--back-color', '255, 255, 255');
+		document.body.style.setProperty('--addit-color', '4, 4, 174');
+		document.body.style.setProperty('--other-color', '0, 0, 0');
+		isDark = false
+	} else {
+		document.body.style.setProperty('--back-color', '0, 0, 0');
+		document.body.style.setProperty('--addit-color', '174, 4, 4');
+		document.body.style.setProperty('--other-color', '217, 217, 217');
+		isDark = true
+	}
+}
