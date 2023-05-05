c3a69c404d53129b324b13bae7d65b4ef5ed812f
mitory <lairziro@gmail.com>
2023-05-05 21:02:26 +0300

    update

---
+++ js/profile_script.js
@@ -0,0 +1,3 @@
+document.body.style.setProperty('--back-color', localStorage.getItem('back_color'));
+document.body.style.setProperty('--addit-color', localStorage.getItem('addit_color'));
+document.body.style.setProperty('--other-color', localStorage.getItem('other_color'));
--- js/script.js
+++ js/script.js
@@ -12,16 +12,29 @@ $(document).ready(function(){
 	});
 });
 let isDark = true
+const back_color_dark = '0, 0, 0',
+		addit_color_dark = '174, 4, 4',
+		other_color_dark = '217, 217, 217',
+		back_color_light = '200, 200, 200',
+		addit_color_light = '50, 50, 174',
+		other_color_light = '27, 27, 27';
 function changeTheme(){
 	if(isDark){
-		document.body.style.setProperty('--back-color', '255, 255, 255');
-		document.body.style.setProperty('--addit-color', '4, 4, 174');
-		document.body.style.setProperty('--other-color', '0, 0, 0');
+		document.body.style.setProperty('--back-color', back_color_light);
+		document.body.style.setProperty('--addit-color', addit_color_light);
+		document.body.style.setProperty('--other-color', other_color_light);
 		isDark = false
+		localStorage.setItem('back_color', back_color_light);
+		localStorage.setItem('addit_color', addit_color_light);
+		localStorage.setItem('other_color', other_color_light);
+
 	} else {
-		document.body.style.setProperty('--back-color', '0, 0, 0');
-		document.body.style.setProperty('--addit-color', '174, 4, 4');
-		document.body.style.setProperty('--other-color', '217, 217, 217');
+		document.body.style.setProperty('--back-color', back_color_dark);
+		document.body.style.setProperty('--addit-color', addit_color_dark);
+		document.body.style.setProperty('--other-color', other_color_dark);
 		isDark = true
+		localStorage.setItem('back_color',back_color_dark);
+		localStorage.setItem('addit_color', addit_color_dark);
+		localStorage.setItem('other_color', other_color_dark);
 	}
 }
